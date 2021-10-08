a110a700ff3878a8aca5755802bbaaf61f67eafb
Jashema Panapa <jcpanapa@gmail.com>
2021-10-08 16:13:19 -0400

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // begin working here 
-
+const main = () => document.querySelector("#main")
+const form = () => document.querySelector("#new-blog-form")
+const h1 = () => document.querySelector("#page-title")
+const titleInput= () => document.querySelector("#title")
+const contentInput= () => document.querySelector("#content")
+const submitButton = () => document.querySelector("#submit-button")
