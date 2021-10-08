e44b31b984d02eaf13fc04da80072f8451da1f35
Jashema Panapa <jcpanapa@gmail.com>
2021-10-08 17:25:02 -0400

    modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -5,3 +5,54 @@ const h1 = () => document.querySelector("#page-title")
 const titleInput= () => document.querySelector("#title")
 const contentInput= () => document.querySelector("#content")
 const submitButton = () => document.querySelector("#submit-button")
+const showFormButton = () => document.querySelector("#show-form")
+const showBlogsButton = () => document.querySelector("#show-blogs")
+const blogsList = () => document.querySelector("#blogs-list")
+
+const emptyMain = () => main().innerHTML = ""
+
+const formTemplate = () => {
+    if (form() === null) {
+        return `
+        <h1 id="page-title">Create New Blog</h1>   
+        <form id="new-blog-form">
+            <label for="title">Title</label>
+            <input type="text" id="title">
+            <label for="content">Content</label>
+            <input type="text" id="content">
+            <input type="submit" value="Create!" id="submit-button">
+        </form>
+ 
+        `
+    } else {
+        return null;
+    }
+}
+
+const blogsTemplate = () => {
+    if (blogsList() === null) {
+        return `
+        <h1 id="page-blogs">Blogs</h1>
+        <ul id="blogs-list"></ul>
+        `
+    } else {
+        return null;
+    }
+}
+
+const handleShowFormClick = (e) => {
+    e.preventDefault();
+    main().innerHTML = formTemplate();
+}
+
+const handleShowBlogsClick = (e) => {
+    e.preventDefault();
+    main().innerHTML = blogsTemplate();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    showFormButton().addEventListener("click", handleShowFormClick);
+    showBlogsButton().addEventListener("click", handleShowBlogsClick);
+})
+//DOMContentLoaded doesn't wait for stylesheets and images
+//but load does.
